89f0ab0a81d3f0122aa01feccc9c37fa9dc6fd82
gonzalonietot <maurogonzalonieto2011@hotmail.com.ar>
2021-04-18 18:01:31 -0300

    Calculadora online

---
+++ src/components/Calculator.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="title-container">
+        <h1 class="title">
+          Calculadora
+        </h1>
+    </div>
+    <div class="container">
+      <div class="input-value">
+        <label for="operationValue" />
+        <input
+            v-model="operationValue"
+            class="operation-value"
+            type="text"
+            name="operationValue"
+            id="operationValue">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  data() {
+    return {
+      operationValue: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .title-container {
+    background-color: #1E88E5;
+    height: 75px;
+    .title {
+      color: #ECEFF1;
+      position: absolute;
+      margin-left: 10px;
+      font-size: 30px;
+    }
+  }
+  .container {
+    .input-value {
+      position: absolute;
+      margin-top: 10px;
+      .operation-value {
+        width: 350px;
+        height: 40px;
+      }
+    }
+  }
+
+</style>
