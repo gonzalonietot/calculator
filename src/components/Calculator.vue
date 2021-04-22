84d183076d1b2e610c3bac9af77540664af3dd61
gonzalonietot <maurogonzalonieto2011@hotmail.com.ar>
2021-04-22 15:13:46 -0300

    add styles

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -17,9 +17,9 @@
             readonly />
       </div>
       <div class="button-container">
-          <button v-for="(numb, index) in numbers" :key="index" class="button"> {{ numb }}</button>
-          <button class="button" style="color: white; background-color: #E53935"> {{ result }}</button>
-          <button v-for="(op, ind) in operators" :key="ind" class="button" style="background-color: #8BC34A; color: white"> {{ op }}</button>
+          <button v-for="(number, index) in numbers" :key="index" class="button"> {{ number }}</button>
+          <button class="button result-button"> {{ result }}</button>
+          <button v-for="op in operators" :key="op.id" class="button operation-button"> {{ op.operator }}</button>
       </div>
       <div class="clean-container">
         <button class="clean"> {{ cleanText }} </button>
@@ -36,7 +36,12 @@ export default {
       operationValue: '',
       calculatorText: 'Calculadora',
       numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '.'],
-      operators: ['/', 'x', '' + '+', '-'],
+      operators: [
+        { id: 1, operator: '/' },
+        { id: 2, operator: 'x' },
+        { id: 3, operator: '+' },
+        { id: 4, operator: '-' }
+      ],
       result: '=',
       cleanText: 'Borrar'
     }
@@ -45,25 +50,31 @@ export default {
 </script>
 
 <style scoped lang="scss">
+  $red: #E53935;
+  $white: #FFFFFF;
+  $blue: #1976D2;
+  $green: #4CAF50;
+
   .container {
     .title-container {
-      background-color: #0277BD;
+      background-color: $blue;
       height: 35px;
       border-radius: 5px;
       .title {
         text-align: center;
-        color: white;
+        color: $white;
+        transform: translate(5px, 5px);
       }
     }
     .input-value, .button-container, .clean-container {
       text-align: center;
       margin-top: 10px;
       .operation-value, .clean {
-        width: 90%;
+        width: 93%;
       }
       .clean {
-        background-color: #0277BD;
-        color: white;
+        background-color: $blue;
+        color: $white;
         height: 35px;
         border-radius: 5px;
       }
@@ -73,10 +84,22 @@ export default {
       .button {
         width: 70px;
         height: 30px;
+        margin: 2px 2px;
       }
-      .button:hover {
-        background-color: #37474F;
-        color: white;
+      .result-button {
+        transition: box-shadow 300ms ease-in-out, color 300ms ease-in-out;
+        background-color: $red;
+        color: $white;
+        &:hover,
+        &:focus {
+          box-shadow: 0 0 40px 40px $green inset;
+          outline: 0;
+        }
+      }
+      .operation-button {
+        transition: box-shadow 300ms ease-in-out, color 300ms ease-in-out;
+        background-color: $green;
+        color: $white;
       }
     }
   }
